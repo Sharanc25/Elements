d3af8a48b2d406d5c14d31ed52291a0173610083
Sharan Chandran <sharanc25@gmail.com>
2017-10-02 15:36:10 +0530

    + ‘MatLab/EdgeDetection-Variables.mat’ + ‘MatLab/EdgeDetection.m’ - ‘New folder/’

---
+++ MatLab/EdgeDetection.m
@@ -0,0 +1,55 @@
+image = ('10.tif');
+
+image_info = imfinfo(image);
+
+I = imread(image);
+I = rgb2gray(I);
+
+% Threshold image - global threshold
+BW = imbinarize(I);
+
+% Invert mask
+BW = imcomplement(BW);
+
+% Create masked image.
+maskedImage = I;
+maskedImage(~BW) = 0;
+
+%{
+BW = edge(BW,'canny');
+
+% Make tempoprary line.
+%BW(end, :) = true;
+% arm is sealed off.  Now we can fill.
+BW = imfill(BW, 'holes');
+% Erase temporary line.
+%BW(end,:) = false;
+%}
+
+BW_out = bwareafilt(BW, 15000);
+
+% Get properties.
+properties = regionprops(BW_out, {'Area', 'Eccentricity', 'EquivDiameter', 'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation', 'Perimeter'});
+
+% Area of region of interest
+roi_area = sum([properties.Area]);
+
+% Area of the whole image
+image_area = image_info.Width * image_info.Height;
+
+% Area Fraction of the 
+area_fraction = (roi_area/image_area)*100;
+
+% Standard Deviation
+standard_deviation = std([properties.Area]);
+
+% Average Area
+avg_area = mean(standard_deviation);
+
+thre
+
+imshow(BW_out);
+
+% The %f in the fprintf function prints the notation in decimals instead of
+% exponentials
+fprintf('Area Fraction = %f. Standard Deviation = %f. \n',area_fraction,standard_deviation);
