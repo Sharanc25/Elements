1135c69ebb9add1913a568ce14b632cf17a20a34
Sharan Chandran <sharanc25@gmail.com>
2017-11-28 20:21:05 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -8,6 +8,10 @@ I = imnoise(I,'salt & pepper',0.0002);
 BW = imbinarize(I);
 BW = imcomplement(BW);
 
+% Create masked image.
+maskedImage = I;
+maskedImage(~BW) = 0;
+
 BW2 = bwmorph(BW,'remove');
 figure
 imshow(BW2);
