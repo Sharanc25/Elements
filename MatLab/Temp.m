44c2a2a73ea87cae03a2a7017dd4d6a257b84910
Sharan Chandran <sharanc25@gmail.com>
2017-11-28 19:41:38 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,11 +1,10 @@
 image = ('C:\Users\Sharan\Downloads\5_005.jpg');
 
-image_info = imfinfo(image);
-
 I = imread(image);
 I = rgb2gray(I);
 
 % Threshold image - global threshold
 BW = imbinarize(I);
-BW = imcomplement(BW);
+%BW = imcomplement(BW);
+
 imshow(BW);
