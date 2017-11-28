45b0b21d74117ed593b2ba10edf01e1f8419e363
Sharan Chandran <sharanc25@gmail.com>
2017-11-28 19:48:56 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -6,6 +6,6 @@ I = rgb2gray(I);
 % Threshold image - global threshold
 BW = imbinarize(I);
 BW = imcomplement(BW);
-BW2 = bwmorph(BW,'remove');
-
-imshow(BW);
+BW2 = bwmorph(BW1,'skel',Inf);
+figure
+imshow(BW2);
