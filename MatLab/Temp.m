6b43e69e64107b196e819b3bcf8e848d5bdaea66
Sharan Chandran <sharanc25@gmail.com>
2017-11-28 19:58:22 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -2,7 +2,7 @@ image = ('6.tif');
 
 I = imread(image);
 I = rgb2gray(I);
-I= imnoise(I,'salt & pepper',0.2);
+I= imnoise(I,'salt & pepper',0.001);
 
 % Threshold image - global threshold
 BW = imbinarize(I);
