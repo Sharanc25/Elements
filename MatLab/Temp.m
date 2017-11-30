6ebd4380dd6e59353baeff313015504fcfded285
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:37:23 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,9 +1,8 @@
-
 files = ([C:\Users\Sharan\Desktop\Ti6242-2-Old-FS, '\*.txt']);
 
-fopen(filename, 'rt');  %for reading
-fopen(filename, 'wt');  %for writing
+%fopen(filename, 'rt');  %for reading
+%fopen(filename, 'wt');  %for writing
 
-system('copy /a C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\combined.txt');
+%system('copy /a C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\combined.txt');
 
 %data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
