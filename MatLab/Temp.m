5bee140ff6ba2125020f6c3c0b908bb0b371cd2c
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:38:51 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,6 +1,6 @@
 files = dir(['C:\Users\Sharan\Desktop\Ti6242-2-Old-FS', '\*.txt']);
 
-disp(files);
+disp(length(files));
 
 %fopen(filename, 'rt');  %for reading
 %fopen(filename, 'wt');  %for writing
