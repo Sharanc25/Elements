a392dd36e39b6cd6dd9fa36451f25e510e0c2fe8
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:01:31 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,2 +1,7 @@
 %system('copy *.txt combined.txt');
 data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
+for k = [1, 2]
+   disp(A.colheaders{1, k})
+   disp(A.data(:, k))
+   disp(' ')
+end
