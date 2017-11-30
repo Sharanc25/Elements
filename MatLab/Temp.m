666af5c50cdeac05f8d08b195682b77cf4e9ff56
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:31:16 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,3 +1,4 @@
+fopen();
 system('copy /a C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\combined.txt');
 
 %data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
