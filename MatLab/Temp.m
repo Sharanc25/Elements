b17c375af3bd2861e5c6efa4f58c99b980cb5f8d
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 19:59:14 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,5 +1,2 @@
 %system('copy *.txt combined.txt');
-fid = fopen('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt', 'rt');
-Data = textscan(fid, '%d %f %f', 'headerLines', 25, 'CollectOutput', true);
-fclose(fid);
-Data= cell2mat(Data);   
+importdata(filename,delimiterIn,headerlinesIn);
