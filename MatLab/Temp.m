b9d3046567cbb091f319eb8354aff7003d380ac6
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:41:12 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -8,4 +8,4 @@ numberOfFiles = length(files);
 
 %system('copy /a C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\combined.txt');
 
-%data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
+data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt',' ',14);
