881a95139fb9e851bb1c28b2bea3c5cfe5bf7a92
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 19:54:48 +0530

    / ‘MatLab/Temp.m’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -2,4 +2,4 @@
 fid = fopen('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt', 'rt');
 Data = textscan(fid, '%d %f %f', 'headerLines', 14, 'CollectOutput', true);
 fclose(fid);
-Data= cell2mat( Data);   
+Data= cell2mat(Data);   
