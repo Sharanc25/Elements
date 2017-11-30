53f74ce52054cf254411a02d7523a87d92380ef0
Sharan Chandran <sharanc25@gmail.com>
2017-11-30 20:16:47 +0530

    / ‘MatLab/Temp.m’ + ‘MatLab/combined.txt’

--- MatLab/Temp.m
+++ MatLab/Temp.m
@@ -1,7 +1,3 @@
-%system('copy *.txt combined.txt');
-data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
-for k = [1, 2]
-   
-   disp(data.data(:, k))
-   disp(' ')
-end
+system('copy C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\*.txt combined.txt');
+
+%data = importdata('C:\Users\Sharan\Desktop\Ti6242-2-Old-FS\1st day.txt',' ',14);
