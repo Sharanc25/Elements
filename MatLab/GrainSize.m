19d5bbf04dfef1867051fddbfa6fed89c835ae25
Sharan Chandran <sharanc25@gmail.com>
2017-12-01 14:41:17 +0530

    + ‘MatLab/GrainSize.m’

---
+++ MatLab/GrainSize.m
@@ -0,0 +1,40 @@
+image = CS1;
+
+image_info = imfinfo(image);
+
+disp(image_info);
+
+%{
+% Invert mask
+BW = imcomplement(BW);
+
+
+
+
+
+BW_out = bwareafilt(BW, 15000);
+
+% Get properties.
+properties = regionprops(BW_out, {'Area', 'Eccentricity', 'EquivDiameter', 'EulerNumber', 'MajorAxisLength', 'MinorAxisLength', 'Orientation', 'Perimeter'});
+
+% Area of region of interest
+roi_area = sum([properties.Area]);
+
+% Area of the whole image
+image_area = image_info.Width * image_info.Height;
+
+% Area Fraction of the 
+area_fraction = (roi_area/image_area)*100;
+
+% Standard Deviation
+standard_deviation = std([properties.Area]);
+
+% Average Area
+avg_area = mean(standard_deviation);
+
+imshow(BW_out);
+
+% The %f in the fprintf function prints the notation in decimals instead of
+% exponentials
+fprintf('Area Fraction = %f. Standard Deviation = %f. \n',area_fraction,standard_deviation);
+%}
